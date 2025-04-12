56cb7d6993eb514acd06eca9301ff08ee5864df2
jancen.rodavia <160316579+Doffy03@users.noreply.github.com>
2025-04-12 17:45:46 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC2TXHybTwX9B5nEoaZm9XFbPGQOkgJGzE",
+  authDomain: "crud-5f27f.firebaseapp.com",
+  databaseURL: "https://crud-5f27f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-5f27f",
+  storageBucket: "crud-5f27f.firebasestorage.app",
+  messagingSenderId: "637073050399",
+  appId: "1:637073050399:web:80a362907e6c0133217e2a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
